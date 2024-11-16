bc8cdd2764f85817b66954ba1160b51c0a756c43
Andrei Manu <896868@stud.unive.it>
2024-11-16 13:25:09 +0100

    Inital commit

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to My Portfolio</h1>
+    <button @click="playGame">Play Game</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    playGame() {
+      console.log('Game starting...');
+    },
+  },
+};
+</script>
