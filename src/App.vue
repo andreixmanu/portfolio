f6a999005a9faf018c47f300caa2824a0b075e36
Andrei Manu <896868@stud.unive.it>
2024-11-17 10:01:29 +0100

    updated project structure

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- Create a basic component to turn the background red -->
+
+<template>
+  <div id="app">
+    <red-background></red-background>
+  </div>
+</template>
+
+<script>
+import RedBackground from './components/RedBackground.vue'
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,9 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
 });
+
+module.exports = {
+    devServer: {
+        proxy: 'http://localhost:8080',
+    },
+};
